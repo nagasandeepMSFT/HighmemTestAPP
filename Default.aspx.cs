ef0447fd98272a612f2e1880684df4f73737015e
Naga Sandeep Ponduru <napondur@microsoft.com>
2023-05-11 13:19:07 -0700

    Add project files.

---
+++ Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Highmem
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void HighMem_Click(object sender, EventArgs e)
+        {
+            _ = new string[1000, 10251, 10];
+        }
+
+  
+    }
+}
